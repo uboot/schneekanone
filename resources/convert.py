77492594219e6d82939c5d0dc9ae6c81592ddd4b
Matthias Fuchs <matz.fuchs@gmx.at>
2016-05-27 11:06:19 +0200

    Decode sprite templates

--- resources/convert.py
+++ resources/convert.py
@@ -1,7 +1,18 @@
 import struct
 
 def readShort(stream):
-    return struct.unpack('>h', stream.read(2))
+    v, = struct.unpack('>h', stream.read(2))
+    return v
+    
+def readLong(stream):
+    v, = struct.unpack('>l', stream.read(4))
+    return v
+    
+def readOSType(stream):
+    data = []
+    for i in range(4):
+        data += struct.unpack('c', stream.read(1))
+    return ''.join(data)
     
 def readFixedSprite(stream):
     data = {
@@ -45,18 +56,39 @@ def readBonusSprite(stream):
     }
     return data
     
-f = open('01001')
-
-fixedSpriteCount, = readShort(f)
-for i in range(fixedSpriteCount):
-    print readFixedSprite(f)
+def readSpriteTemplate(stream):
+    data = {
+        'id': readOSType(stream),
+        'horzPosition': readShort(stream),
+        'vertPosition': readShort(stream),
+        'moveDelay': readLong(stream),
+        'moveDelayMult': readLong(stream),
+        'celDelay': readLong(stream),
+        'celDelayMult': readLong(stream),
+        'visible': readShort(stream),
+        'celIndex': readShort(stream),
+        'celCount': readShort(stream),
+        'cicnIDs': readShort(stream)
+    }
+    return data
     
-moveSpriteCount, = readShort(f)
-for i in range(moveSpriteCount):
-    print readMoveSprite(f)
+with open('01001') as f:
+    fixedSpriteCount = readShort(f)
+    for i in range(fixedSpriteCount):
+        print readFixedSprite(f)
+        
+    moveSpriteCount = readShort(f)
+    for i in range(moveSpriteCount):
+        print readMoveSprite(f)
+        
+    bonusSpriteCount = readShort(f)
+    for i in range(bonusSpriteCount):
+        print readBonusSprite(f)
+    print len(f.read())
     
-bonusSpriteCount, = readShort(f)
-for i in range(bonusSpriteCount):
-    print readBonusSprite(f)
+with open('00200') as f:
+    print readSpriteTemplate(f)
     
-print len(f.read())
+with open('00205') as f:
+    print readSpriteTemplate(f)
+    
